e0089d1bca02eb5f422e0052e6233d0786e0f4e5
DhritiShah04 <162575008+DhritiShah04@users.noreply.github.com>
2025-08-29 00:23:23 +0530

    Update 23510083_A1.cpp
    updated code

--- 23510083_A1.cpp
+++ 23510083_A1.cpp
@@ -8,14 +8,18 @@ int main() {
     cin>>n;
     omp_set_num_threads(n);
 
+    double start_t1 = omp_get_wtime();
     //Sequential
     cout << "Threads printed Sequentially!" <<endl;
     for(int i=0;i<n;i++){
         cout << "Hello World from thread " << i << " out of " << n << " threads!" << endl;
     }
-
+    double end_t1 = omp_get_wtime();
+    double time1 = (end_t1 - start_t1);
     cout << endl;
+
     //Parallel
+    double start_t2 = omp_get_wtime();
     cout << "Threads printed parallelly!" <<endl;
     #pragma omp parallel
     {
@@ -23,5 +27,14 @@ int main() {
         int total_threads = omp_get_num_threads();
         cout << "Hello World from thread " << thread_id << " out of " << total_threads << " threads!" << endl;
     }
+    double end_t2 = omp_get_wtime();
+    double time2 = (end_t2 - start_t2);
+    
+    cout << "Execution Time for Sequential: " << time1 << " seconds" << endl;
+    cout << "Execution Time for Parallel: " << time2 << " seconds" << endl;
+
+    double speedup_time = time1 / time2;
+    cout << "Speed-Up time is: " << speedup_time << endl;
+
     return 0;
 }
